3ca43e2447b576ac1dd03e7ab03c94baf488197f
Raean Chrissean Tamayo <143667091+Tamagomago@users.noreply.github.com>
2024-12-03 19:50:57 +0800

    Finished Layout For Frontline Employee

--- src/LogisticsUI/FrontlineEmployee.java
+++ src/LogisticsUI/FrontlineEmployee.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
 package LogisticsUI;
+import java.awt.CardLayout;
 
 /**
  *
@@ -26,93 +27,206 @@ public class FrontlineEmployee extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanel1 = new javax.swing.JPanel();
-        Sidebar = new javax.swing.JPanel();
-        Greetings = new javax.swing.JLabel();
+        NullPanel = new javax.swing.JPanel();
+        SidebarPanel = new javax.swing.JPanel();
+        UserGreetingsLabel = new javax.swing.JLabel();
+        Logout = new javax.swing.JButton();
+        ProcessShipmentBtn = new javax.swing.JButton();
+        MainPanel = new javax.swing.JPanel();
+        EmptyPanel = new javax.swing.JPanel();
+        ProcessShipmentPanel = new javax.swing.JPanel();
+        NestedPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        PaidShipmentsTable = new javax.swing.JTable();
         jButton1 = new javax.swing.JButton();
-        Dashboard = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMaximizedBounds(new java.awt.Rectangle(0, 0, 800, 500));
+        setMaximumSize(new java.awt.Dimension(800, 500));
+        setMinimumSize(new java.awt.Dimension(800, 500));
+        setPreferredSize(new java.awt.Dimension(800, 500));
 
-        jPanel1.setLayout(null);
+        NullPanel.setBackground(new java.awt.Color(240, 245, 255));
+        NullPanel.setLayout(null);
 
-        Sidebar.setBackground(new java.awt.Color(70, 92, 239));
+        SidebarPanel.setBackground(new java.awt.Color(70, 92, 239));
 
-        Greetings.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
-        Greetings.setForeground(new java.awt.Color(255, 255, 255));
-        Greetings.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        Greetings.setText("<html><span style='font-family:Inter; font-weight:bold;'>Hello</span>, <span style='font-family:Inter;'>Employee</span></html>");
+        UserGreetingsLabel.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        UserGreetingsLabel.setForeground(new java.awt.Color(255, 255, 255));
+        UserGreetingsLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        UserGreetingsLabel.setText("<html><span style='font-family:Inter; font-weight:bold;'>Hello, </span><span style='font-family:Inter; font-weight:50;'>Employee</span></html>");
 
-        jButton1.setBackground(new java.awt.Color(70, 92, 239));
-        jButton1.setFont(new java.awt.Font("Segoe UI Emoji", 0, 20)); // NOI18N
-        jButton1.setForeground(new java.awt.Color(255, 255, 255));
-        jButton1.setText("<html><span style='font-family:Inter; font-weight:100;'>Process Shipment</span></html>");
-        jButton1.setBorder(null);
-        jButton1.setBorderPainted(false);
-        jButton1.setContentAreaFilled(false);
-        jButton1.setFocusPainted(false);
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        Logout.setBackground(new java.awt.Color(80, 155, 229));
+        Logout.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        Logout.setForeground(new java.awt.Color(255, 255, 255));
+        Logout.setText("<html>   <body style=\"font-family: Inter; font-weight: 600; text-align: center;\">     Logout   </body> </html> ");
+        Logout.setBorderPainted(false);
+        Logout.setFocusPainted(false);
+        Logout.setFocusable(false);
+        Logout.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        Logout.setRequestFocusEnabled(false);
+        Logout.setRolloverEnabled(false);
+        Logout.setVerifyInputWhenFocusTarget(false);
+        Logout.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                LogoutActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout SidebarLayout = new javax.swing.GroupLayout(Sidebar);
-        Sidebar.setLayout(SidebarLayout);
-        SidebarLayout.setHorizontalGroup(
-            SidebarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(SidebarLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(Greetings, javax.swing.GroupLayout.DEFAULT_SIZE, 239, Short.MAX_VALUE)
-                .addContainerGap())
-            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING)
+        ProcessShipmentBtn.setBackground(new java.awt.Color(70, 92, 239));
+        ProcessShipmentBtn.setFont(new java.awt.Font("Segoe UI", 0, 20)); // NOI18N
+        ProcessShipmentBtn.setForeground(new java.awt.Color(255, 255, 255));
+        ProcessShipmentBtn.setText("<html><body style=\"font-family: Inter; font-weight: 10; text-align: center;\">Process Shipment</body></html>");
+        ProcessShipmentBtn.setBorderPainted(false);
+        ProcessShipmentBtn.setContentAreaFilled(false);
+        ProcessShipmentBtn.setFocusPainted(false);
+        ProcessShipmentBtn.setFocusable(false);
+        ProcessShipmentBtn.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        ProcessShipmentBtn.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                ProcessShipmentBtnActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout SidebarPanelLayout = new javax.swing.GroupLayout(SidebarPanel);
+        SidebarPanel.setLayout(SidebarPanelLayout);
+        SidebarPanelLayout.setHorizontalGroup(
+            SidebarPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(SidebarPanelLayout.createSequentialGroup()
+                .addGap(6, 6, 6)
+                .addComponent(UserGreetingsLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(ProcessShipmentBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(SidebarPanelLayout.createSequentialGroup()
+                .addGap(33, 33, 33)
+                .addComponent(Logout, javax.swing.GroupLayout.PREFERRED_SIZE, 137, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
-        SidebarLayout.setVerticalGroup(
-            SidebarLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(SidebarLayout.createSequentialGroup()
-                .addGap(47, 47, 47)
-                .addComponent(Greetings, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(49, 49, 49)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(292, Short.MAX_VALUE))
+        SidebarPanelLayout.setVerticalGroup(
+            SidebarPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(SidebarPanelLayout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(UserGreetingsLabel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(70, 70, 70)
+                .addComponent(ProcessShipmentBtn, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(163, 163, 163)
+                .addComponent(Logout, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
-        jPanel1.add(Sidebar);
-        Sidebar.setBounds(0, 0, 251, 500);
+        NullPanel.add(SidebarPanel);
+        SidebarPanel.setBounds(0, 0, 200, 500);
 
-        Dashboard.setBackground(new java.awt.Color(240, 245, 255));
+        MainPanel.setBackground(new java.awt.Color(240, 245, 255));
+        MainPanel.setLayout(new java.awt.CardLayout());
 
-        javax.swing.GroupLayout DashboardLayout = new javax.swing.GroupLayout(Dashboard);
-        Dashboard.setLayout(DashboardLayout);
-        DashboardLayout.setHorizontalGroup(
-            DashboardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
+        EmptyPanel.setBackground(new java.awt.Color(240, 245, 255));
+        EmptyPanel.setLayout(new java.awt.CardLayout());
+        MainPanel.add(EmptyPanel, "card2");
+
+        ProcessShipmentPanel.setBackground(new java.awt.Color(240, 245, 255));
+        ProcessShipmentPanel.setLayout(new java.awt.CardLayout());
+
+        NestedPanel.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 24)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel1.setText("<html><span style='font-family: Inter; font-weight: bold;'>Unprocessed Paid Shipment(s)</span></html>");
+
+        PaidShipmentsTable.setBackground(new java.awt.Color(255, 255, 255));
+        PaidShipmentsTable.setForeground(new java.awt.Color(0, 0, 0));
+        PaidShipmentsTable.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+
+            },
+            new String [] {
+                "Shipment No.", "ID", "Status"
+            }
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.Integer.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
+        PaidShipmentsTable.setCellSelectionEnabled(true);
+        PaidShipmentsTable.setFocusable(false);
+        PaidShipmentsTable.setGridColor(new java.awt.Color(255, 255, 255));
+        PaidShipmentsTable.setSelectionBackground(new java.awt.Color(204, 204, 204));
+        PaidShipmentsTable.setSelectionForeground(new java.awt.Color(255, 255, 255));
+        PaidShipmentsTable.setShowGrid(false);
+        PaidShipmentsTable.getTableHeader().setResizingAllowed(false);
+        PaidShipmentsTable.getTableHeader().setReorderingAllowed(false);
+        jScrollPane1.setViewportView(PaidShipmentsTable);
+        PaidShipmentsTable.getColumnModel().getSelectionModel().setSelectionMode(javax.swing.ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+
+        jButton1.setBackground(new java.awt.Color(73, 204, 112));
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("<html>\n  <body style=\"font-family: Inter; font-weight: bold; text-align: center;\">\n    Confirm\n  </body>\n</html>\n");
+        jButton1.setBorderPainted(false);
+        jButton1.setFocusPainted(false);
+        jButton1.setFocusable(false);
+        jButton1.setRequestFocusEnabled(false);
+        jButton1.setRolloverEnabled(false);
+
+        javax.swing.GroupLayout NestedPanelLayout = new javax.swing.GroupLayout(NestedPanel);
+        NestedPanel.setLayout(NestedPanelLayout);
+        NestedPanelLayout.setHorizontalGroup(
+            NestedPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(NestedPanelLayout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(NestedPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 103, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(NestedPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 536, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(37, Short.MAX_VALUE))
         );
-        DashboardLayout.setVerticalGroup(
-            DashboardLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 500, Short.MAX_VALUE)
+        NestedPanelLayout.setVerticalGroup(
+            NestedPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(NestedPanelLayout.createSequentialGroup()
+                .addGap(60, 60, 60)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 306, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(64, Short.MAX_VALUE))
         );
 
-        jPanel1.add(Dashboard);
-        Dashboard.setBounds(200, 0, 600, 500);
+        ProcessShipmentPanel.add(NestedPanel, "card2");
+
+        MainPanel.add(ProcessShipmentPanel, "card3");
+
+        NullPanel.add(MainPanel);
+        MainPanel.setBounds(200, 0, 600, 500);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(NullPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(NullPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        jButton1.setBackground(new java.awt.Color(80, 155, 229)); 
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void ProcessShipmentBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ProcessShipmentBtnActionPerformed
+        // TODO add your handling code here:
+        ProcessShipmentBtn.setOpaque(true);
+        ProcessShipmentBtn.setBackground(java.awt.Color.decode("#509BE5"));
+        CardLayout card = (CardLayout)MainPanel.getLayout();
+        card.show(MainPanel, "card3");
+    }//GEN-LAST:event_ProcessShipmentBtnActionPerformed
+
+    private void LogoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_LogoutActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_LogoutActionPerformed
 
     /**
      * @param args the command line arguments
@@ -150,10 +264,18 @@ public class FrontlineEmployee extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel Dashboard;
-    private javax.swing.JLabel Greetings;
-    private javax.swing.JPanel Sidebar;
+    private javax.swing.JPanel EmptyPanel;
+    private javax.swing.JButton Logout;
+    private javax.swing.JPanel MainPanel;
+    private javax.swing.JPanel NestedPanel;
+    private javax.swing.JPanel NullPanel;
+    private javax.swing.JTable PaidShipmentsTable;
+    private javax.swing.JButton ProcessShipmentBtn;
+    private javax.swing.JPanel ProcessShipmentPanel;
+    private javax.swing.JPanel SidebarPanel;
+    private javax.swing.JLabel UserGreetingsLabel;
     private javax.swing.JButton jButton1;
-    private javax.swing.JPanel jPanel1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
     // End of variables declaration//GEN-END:variables
 }
