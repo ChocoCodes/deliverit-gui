8fd5ad3599207cf5610ba3e5ad4d7b75e1ad056f
ChocoCodes <chocojhong999@gmail.com>
2024-12-07 23:32:01 +0800

    Fix: Table Layout

--- src/LogisticsUI/CustomerDashboard.java
+++ src/LogisticsUI/CustomerDashboard.java
@@ -38,7 +38,7 @@ public class CustomerDashboard extends javax.swing.JFrame {
         shipmentsPanel = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
+        shipmentHistTbl = new javax.swing.JTable();
         sendPackagePanel = new javax.swing.JPanel();
         pkgHdr = new javax.swing.JLabel();
         itemTblScroll = new javax.swing.JScrollPane();
@@ -174,18 +174,15 @@ public class CustomerDashboard extends javax.swing.JFrame {
         jLabel2.setFont(new java.awt.Font("Inter", 1, 36)); // NOI18N
         jLabel2.setText("Shipment History");
 
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+        shipmentHistTbl.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+
             },
             new String [] {
                 "Title 1", "Title 2", "Title 3", "Title 4"
             }
         ));
-        jScrollPane1.setViewportView(jTable1);
+        jScrollPane1.setViewportView(shipmentHistTbl);
 
         javax.swing.GroupLayout shipmentsPanelLayout = new javax.swing.GroupLayout(shipmentsPanel);
         shipmentsPanel.setLayout(shipmentsPanelLayout);
@@ -477,7 +474,6 @@ public class CustomerDashboard extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     private javax.swing.JPanel layoutPanel;
     private javax.swing.JButton nameEditBtn;
     private javax.swing.JTextField nameFld;
@@ -494,6 +490,7 @@ public class CustomerDashboard extends javax.swing.JFrame {
     private javax.swing.JPanel sendPackagePanel;
     private javax.swing.JButton sendPkgBtn;
     private javax.swing.JButton shipmentBtn;
+    private javax.swing.JTable shipmentHistTbl;
     private javax.swing.JPanel shipmentsPanel;
     private javax.swing.JPanel sideBarLayout;
     private javax.swing.JButton signOutBtn1;
